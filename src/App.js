2ed37b78c99742fd92ab0507fde29bb9cf03cf0a
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-30 21:45:25 +0200

    <div className="App">

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hello new World!</div>;
+  return (
+  <div className="App">
+    Hello new World!
+    </div>
+  )
 }
 
 export default App;
