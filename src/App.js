84f2b1f7cfc22d8a9ef67e2ee3b10ca292949825
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-11 20:15:19 +0100

    Delete Option

--- src/App.js
+++ src/App.js
@@ -8,64 +8,85 @@ import { Missing } from "./components/Missing";
 import { Nav } from "./components/Nav";
 import { NewPost } from "./components/NewPost";
 import { PostPage } from "./components/PostPage";
-import {format} from 'date-fns'
-import api from './api/posts'
+import { format } from "date-fns";
+import api from "./api/posts";
 import "./App.scss";
+import { EditPost } from "./EditPost";
 
-//! 6:16
+//! 6:36
 function App() {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
   const history = useHistory();
 
-useEffect(() => {
-const fetchPosts= async ()=> {
-  try {
-    const response = await api.get('/posts')
-    setPosts(response.data)
-  } catch (err) {
-    if (err.response) {
-      // Not in the 200 response range
-      console.log(err.response.data);
-      console.log(err.response.status);
-      console.log(err.response.headers);
-    } else {
-      console.log(`Error: ${err.message}`);
-    }
-  }
-}
-fetchPosts();
-}, [])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    };
+    fetchPosts();
+  }, []);
 
   useEffect(() => {
-const filteredResults= posts.filter(post => 
-  ((post.body).toLowerCase()).includes(search.toLowerCase())
-  || ((post.title).toLowerCase()).includes(search.toLowerCase()))
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
 
-  setSearchResults(filteredResults.reverse())
-  }, [posts, search])
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     // const datetime = format(new Date(), "yyyy-MM-dd");
     const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const newPost= {id, title: postTitle, datetime, body: postBody}
+    const newPost = { id, title: postTitle, datetime, body: postBody };
     try {
-      const response = await api.post('/posts', newPost)
+      const response = await api.post("/posts", newPost);
       const allPosts = [...posts, response.data];
       setPosts(allPosts);
-      setPostTitle('');
-      setPostBody('');
-      history.push('/');
+      setPostTitle("");
+      setPostBody("");
+      history.push("/");
     } catch (err) {
       console.log(`Error: ${err.message}`);
     }
   };
-  
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditTitle("");
+      setEditBody("");
+      history.push("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
   const handleDelete = async (id) => {
     try {
       await api.delete(`/posts/${id}`);
@@ -92,6 +113,16 @@ const filteredResults= posts.filter(post =>
             setPostTitle={setPostTitle}
             postBody={postBody}
             setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/edit/:id">
+          <EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
           />
         </Route>
         <Route path="/post/:id">
